03e84dbf80379fdaa5d2376ff495f82483c4bbb8
Aldin Ugljanin <aldinugljaninn@gmail.com>
2023-11-13 21:19:20 +0100

    Finished Forms

---
+++ website/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Member
+
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = [
+            "fname",
+            "lname",
+            "age",
+            "email",
+            "passwd"
+        ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Member
+from .forms import MemberForm
 
 # Create your views here.
 def home(request):
     all_members = Member.objects.all
     return render(request, "website/home.html", {"members": all_members})
+
+def join(request):
+    if request.method =="POST":
+        form = MemberForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return render(request, 'website/join.html', {}) 
+    else:
+        return render(request, 'website/join.html', {})
